b51aaef173786bee13e6f9674fc91efe2beb1359
Boomerang <boomerang@trafficstars.com>
2018-03-31 17:19:36 +0300

    max_page_sie increased

--- comments/urls.py
+++ comments/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
+from django.urls import path, re_path
 
 from comments.views import CommentsListView
+from comments.views.download_comments import download_comments
+from comments.views.get_users_comments import UsersCommentsListView
 
+
+print(download_comments)
 urlpatterns = [
     url(r'^$', CommentsListView.as_view()),
+    path('users/<int:id>/', UsersCommentsListView.as_view(), name='list_view'),
+    path('download/comments.<format>', download_comments),
 ]
--- comments/views/get_comments.py
+++ comments/views/get_comments.py
@@ -10,7 +10,7 @@ from comments.serializers.comment import CommentSerializer
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 100
     page_size_query_param = 'page_size'
-    max_page_size = 1000
+    max_page_size = 20000
 
 
 class CommentsListView(generics.ListCreateAPIView):
