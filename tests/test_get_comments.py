596a0597ea1d8c17809f021e9fdf1d50a138d669
Boomerang <boomerang@trafficstars.com>
2018-03-31 14:17:56 +0300

    tests

--- tests/test_get_comments.py
+++ tests/test_get_comments.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
+
+from comments.models import CommentMessage
 from users.models import User
 
 class UsersTestCase(TestCase):
     def setUp(self):
-        User.objects.create()
-        User.objects.create()
+        user = User.objects.create()
+        root_comment = CommentMessage.objects.create(text="Test text", user=user, parent_id=1, parent_type="article")
+        child_comment = CommentMessage.objects.create(text="Test text", user=user, parent_id=1, parent_type="comment")
 
     def test_animals_can_speak(self):
         first = User.objects.get(pk=1)
